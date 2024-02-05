988e7a4f54a64d105bdf227f4fc1de2094eb79e2
vyazemskaya <vyazemskayanna@gmail.com>
2024-02-06 02:51:15 +0300

    update

--- src/js/utils/modals.js
+++ src/js/utils/modals.js
@@ -404,13 +404,6 @@ class Modal {
   }
 }
 
-modules.modal = new Modal({});
+// --------------------------------------------------------------------------
 
-// show age modal
-if (document.querySelector('.mainpage')) {
-  const confirmAgeBtn = document.getElementById('confirm-age-btn');
-  // modules.modal.open('#confirmAgeModal');
-  confirmAgeBtn.addEventListener('click', function () {
-    modules.modal.close('#confirmAgeModal');
-  });
-}
+modules.modal = new Modal({});
